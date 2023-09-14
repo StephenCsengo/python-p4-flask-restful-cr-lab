d82977b9becda4f08714b62763aeb4805471f394
Stephen Csengo <stephencsengo@gmail.com>
2023-09-14 16:23:47 -0400

    Lab complete

--- server/app.py
+++ server/app.py
@@ -7,8 +7,8 @@ from flask_restful import Api, Resource
 from models import db, Plant
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///plants.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = True
 
 migrate = Migrate(app, db)
@@ -16,12 +16,54 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class Plants(Resource):
-    pass
+    def get(self):
+        plant_dict_list = [plant.to_dict() for plant in Plant.query.all()]
+
+        response = make_response(
+            jsonify(plant_dict_list),
+            200,
+        )
+        return response
+
+    def post(self):
+        plant_data = request.get_json()
+
+        new_plant = Plant(
+            name=plant_data["name"],
+            image=plant_data["image"],
+            price=plant_data["price"],
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        new_plant_dict = new_plant.to_dict()
+
+        response = make_response(
+            jsonify(new_plant_dict),
+            201,
+        )
+        return response
+
+
+api.add_resource(Plants, "/plants")
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant_dict = Plant.query.filter_by(id=id).first().to_dict()
 
-if __name__ == '__main__':
+        response = make_response(
+            jsonify(plant_dict),
+            200,
+        )
+        return response
+
+
+api.add_resource(PlantByID, "/plants/<int:id>")
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
